e281538fbb46ca4682e77b315740e7bd7cc3a609
John Martinsson <john.martinsson@gmail.com>
2025-03-31 17:48:36 +0200

    Update playback_audio.py

---
+++ akulab-2025/playback-laptop/playback_audio.py
@@ -0,0 +1,4 @@
+# TODO
+# - Speak at different intensities inside of the Auklab to understand if and when that speech signal is picked up by the microphones
+# - Payback different sound events at the ledges
+# - Write a script that does some or most of this. Maybe add the "speech inside" and "events ledges" scenarios, so we can playback them when there.
